ba4a2c0b7cb8ef9b65419214419f6cf1f9379494
Vladimir Keleshev <vladimir@keleshev.com>
2023-01-11 23:17:28 +0100

    Straight-line code type inference

--- docopt.ml
+++ docopt.ml
@@ -5,7 +5,13 @@ let hello = "world"
 
 let (let*) = Result.bind
 
-module Term = struct
+module Env = struct 
+  include Map.Make (String)
+
+  let to_list m = to_seq m |> List.of_seq
+end
+
+module Doc = struct
   type option =
     | Long of string
     | Short of char
@@ -13,19 +19,41 @@ module Term = struct
   type t =
     | Command of string
     | Argument of string
+  (*| Option of option * string option*)
+    (*| Atom of atom*)
     | Sequence of t * t
-  (*| Option of option * string option
     | Optional of t
-    | Either of t * t
-    | One_or_more of t *)
+  (*uEither of t * t*)
+    | One_or_more of t
+
+  type 't t_open = [
+    | `Command of string
+    | `Argument of string
+    | `Sequence of 't * 't
+    | `Optional of 't
+    | `One_or_more of 't
+  ]
+
+  type t' = [
+    t' t_open
+    | `x
+  ]
+
+  module Full = struct
+    type t = [
+      t t_open
+      | `Either of t * t
+    ]
+  end
 end
 
 module Type = struct
   type _ t =
     | String: string t
     | Int: int t
-    | Unit: unit t
-    | Pair: 'a t * 'b t -> ('a * 'b) t
+    | Bool: bool t
+  (*| Unit: unit t*)
+  (*| Pair: 'a t * 'b t -> ('a * 'b) t*)
 (*  | Bool
     | Enum of ...
     | Option of t
@@ -36,38 +64,93 @@ module Type = struct
     match int_of_string_opt source with
     | Some n -> Ok n
     | None -> Error (`Can't_parse_x_expected_type (source, Int))
+
+  module Dynamic = struct
+    (*type scalar = Unit | String
+
+    type t =
+      | Scalar of scalar
+      | Option of scalar
+      | List of scalar*)
+
+    type collection = Scalar | Option | List
+    type base = Unit | String
+    type t = base * collection
+
+    let promote (left, _) (right, _) =
+      match left, right with
+      | Unit, Unit -> Unit, List
+      | String, String -> String, List
+      | Unit, String 
+      | String, Unit -> failwith "incompatible types"
+  end
 end
 
+
+let rec infer = function
+  | Doc.Argument a -> Env.singleton a Type.Dynamic.(String, Scalar)
+  | Doc.Command c -> Env.singleton c Type.Dynamic.(Unit, Scalar)
+  | Doc.Sequence (left, right) -> 
+      let env1 = infer left in
+      let env2 = infer right in
+      let merger _key (left: Type.Dynamic.t option) (right: Type.Dynamic.t option): Type.Dynamic.t option =
+        match left, right with
+        | Some left_t, None -> Some left_t
+        | None, Some right_t -> Some right_t
+        | None, None -> None
+        | Some left_t, Some right_t -> Some (Type.Dynamic.promote left_t right_t)
+      in
+      Env.merge merger env1 env2
+  | Doc.Optional doc ->
+      let env = infer doc in
+      let mapper: Type.Dynamic.t -> Type.Dynamic.t = function
+        | x, Scalar -> x, Option
+        | t -> t
+      in
+      Env.map mapper env
+  | Doc.One_or_more doc ->
+      let env = infer doc in
+      Env.map (function x, _ -> x, Type.Dynamic.List) env
+
+
 type _ t =
-  | Term: 'a Type.t * Term.t -> 'a t
+  | Term: 'a Type.t * string -> 'a t
   | Map: ('a -> 'b) * 'a t -> 'b t
+  | Both: 'a t * 'b t -> ('a * 'b) t
 
-let term term_t term = Term (term_t, term)
-let map term callback = Map (term, callback)
+let term term_t atom = Term (term_t, atom)
+let map callback term = Map (callback, term)
+let both first second = Both (first, second)
 
-(*let rec eval (type a) ~(argv: string list) (term: a t): (a, _) result =*)
 
 let rec eval
-  : type a. argv:string list -> a t -> (a, _) result
+  : type a. argv:string list -> a t -> (a * string list, _) result
   = fun ~argv -> function
 
-  | Term (Type.String, Term.Argument a) -> begin
+  | Term (Type.String, a) -> begin
       match argv with
-      | head :: _ -> Ok head
+      | head :: tail -> Ok (head, tail)
       | [] -> Error (`Missing_positional_argument a)
     end
 
-  | Term (Type.Int, (Argument _ as term)) ->
-      let term = Term (Type.String, term) in
-      let* result = eval ~argv (map Type.int_of_string term) in
-      result
-(*
-  | Term (Type.Pair (left_t, right_t), Sequence (left, right)) ->
-      let* result = eval ~argv (Term (left_t, left)) in
-  *)    
+  | Term (Type.Int, doc) ->
+      let term = Term (Type.String, doc) in
+      let* value, tail = eval ~argv (map Type.int_of_string term) in
+      let* value in
+      Ok (value, tail)
+
+  (*| Term (Type.Pair (left_t, right_t), Sequence (left, right)) ->
+      let* left_value, argv = eval ~argv (Term (left_t, left)) in
+      let* right_value, argv = eval ~argv (Term (right_t, right)) in
+      Ok ((left_value, right_value), argv)*)
 
   | Map (callback, term) ->
-      let* result = eval ~argv term in
-      Ok (callback result)
-      
-  | _ -> failwith "not implemented"
+      let* value, tail = eval ~argv term in
+      Ok (callback value, tail)
+
+  | Both (left, right) ->
+      let* left_value, argv = eval ~argv left in
+      let* right_value, argv = eval ~argv right in
+      Ok ((left_value, right_value), argv)
+
+  | _ -> failwith "not implemented"      
--- docopt_test.ml
+++ docopt_test.ml
@@ -1,28 +1,89 @@
 let (=>) left right = print_char (if left = right then '.' else 'F')
 
 
+let (let+) term callback = Docopt.map callback term
+let (and+) = Docopt.both
 
 
 
 let _eval_string_term = 
-  let term = Docopt.(term Type.String Term.(Argument "<x>")) in
-  Docopt.eval term ~argv:["42"] => Ok "42"
+  let term = Docopt.(term Type.String "<x>") in
+  Docopt.eval term ~argv:["42"] => Ok ("42", [])
 
 let _eval_int_term = 
-  let term = Docopt.(term Type.Int Term.(Argument "<x>")) in
-  Docopt.eval term ~argv:["42"] => Ok 42
+  let term = Docopt.(term Type.Int "<x>") in
+  Docopt.eval term ~argv:["42"] => Ok (42, [])
 
 let _map_string_to_int =
   let term = 
-    Docopt.(term Type.String Term.(Argument "<x>")) 
+    Docopt.(term Type.String "<x>") 
     |> Docopt.map int_of_string
   in
-  Docopt.eval term ~argv:["42"] => Ok 42 
+  Docopt.eval term ~argv:["42"] => Ok (42, [])
+
+let _map_string_to_int_using_let_syntax =
+  let term = 
+    let+ x = Docopt.(term Type.String "<x>") in
+    `Return (int_of_string x)
+  in
+  Docopt.eval term ~argv:["42"] => Ok (`Return 42, [])
+
+(*let _test =
+  let doc = Docopt.Doc.(Atom (Argument "<x>")) in
+  let term =
+    let+ x = Docopt.(term Type.Int Doc.(Argument "<x>")) in
+    `Return x
+  in
+  Docopt.eval' term ~doc ~argv:["42"] => Ok (`Return 42)*)
+
+let _infer_type =
+  let doc = Docopt.Doc.(Sequence (Argument "<x>", Command "a")) in
+  Docopt.infer doc |> Docopt.Env.to_list => [
+    "<x>", Docopt.Type.Dynamic.(String, Scalar);
+    "a", Docopt.Type.Dynamic.(Unit, Scalar );
+  ]
+
+let _infer_repeating_sequence =
+  let doc = Docopt.Doc.(Sequence (Argument "<x>", Argument "<x>")) in
+  Docopt.infer doc |> Docopt.Env.to_list => [
+    "<x>", Docopt.Type.Dynamic.(String, List);
+  ]
+
+let _infer_optional_sequence =
+  let doc = Docopt.Doc.(Optional (Argument "<x>")) in
+  Docopt.infer doc |> Docopt.Env.to_list => [
+    "<x>", Docopt.Type.Dynamic.(String, Option);
+  ]
+
+let _infer_repeating_argument =
+  let doc = Docopt.Doc.(One_or_more (Argument "<x>")) in
+  Docopt.infer doc |> Docopt.Env.to_list => [
+    "<x>", Docopt.Type.Dynamic.(String, List);
+  ]
+(*let _infer_type =
+  let doc = "Usage: prog a [b] c... <x> [<y>] <z>..." in
+  let argv = ["set"; "1"; "2"] in
+  Docopt.eval ~doc ~argv => Ok [
+    "a", Unit;
+    "b", Bool;
+    "a", Int;
+    "<x>", String;
+    "<y>", Option String;
+    "<z>", List1 String;
+  ]*)
 
 (*let _eval_sequence_as_pair =
-  let term = Docopt.(term Type.(Pair (String, Int)) Term.(Sequence (Argument "<x>", Argument "<y>"))) in
-  Docopt.eval term ~argv:["1"; "2"] => Ok ("1", 2)*)
+  let syntax = Docopt.Doc.(Sequence (Argument "<x>", Argument "<y>")) in
+  let term = Docopt.(term Type.(Pair (String, Int)) syntax) in
+  Docopt.eval term ~argv:["1"; "2"] => Ok (("1", 2), [])*)
 
+let _eval_sequence_as_two_bindings =
+  let term =
+    let+ first = Docopt.(term Type.String "<x>")
+    and+ second = Docopt.(term Type.Int "<y>") in
+    `Return (first, second)
+  in
+  Docopt.eval term ~argv:["1"; "2"] => Ok (`Return ("1", 2), [])
 
 (*
 let (let+), (and+) = Docopt.(map, product)
